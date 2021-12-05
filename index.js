2b8ea39d03a0f9acb92424a42b20292e530c897f
Olek Myron <myrondell@gmail.com>
2021-12-05 09:29:21 +0200

    done hw 2

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
-const [btnRed, btnBlue] = document.querySelectorAll('.btn');
-
-btnRed.addEventListener('click', parseNumber);
-btnBlue.addEventListener('click', addDivideNumbers);
-
-function isInvalid(arg) {
-  const parsedArg = Number(arg);
-  return (
-    typeof parsedArg !== 'number' ||
-    isNaN(parsedArg) ||
-    arg === null ||
-    arg === '' ||
-    arg.trim() === ''
-  );
-}
-
 // --- First Task ---
-function parseNumber() {
-  const int = prompt('Enter a number');
-  const calc = prompt('Enter a system number');
+function makeObjectDeepCopy(obj) {
+  if (Array.isArray(obj)) {
+    return obj.map(el => (el = makeObjectDeepCopy(el)));
+  } else if (typeof obj === 'object') {
+    let clone = {};
 
-  const [parsedInt, parsedCalc] = [Number(int), Number(calc)];
-
-  if (isInvalid(int) || isInvalid(calc)) {
-    return alert('Incorrect input!');
+    for (const key in obj) {
+      clone[key] = makeObjectDeepCopy(obj[key]);
+    }
+    return clone;
+  } else {
+    return obj;
   }
-
-  return alert(parsedInt.toString(parsedCalc));
 }
 
 // --- Second Task ---
-function addDivideNumbers() {
-  const firstArg = prompt('Enter a first number');
-  if (isInvalid(firstArg)) return alert('Incorrect input!');
-  const secondArg = prompt('Enter a second number');
-  if (isInvalid(secondArg)) return alert('Incorrect input!');
+const isInvalidArgs = array => array.some(number => typeof number !== 'number' || isNaN(number));
 
-  const [firstNum, secondNum] = [Number(firstArg), Number(secondArg)];
-  const sum = firstNum + secondNum;
-  const quotient = firstNum / secondNum;
+function selectFromInterval(array, num1, num2) {
+  let from = null;
+  let to = null;
 
-  alert(`Result: ${sum}, ${quotient}`);
+  if (!Array.isArray(array) || isInvalidArgs(array) || isInvalidArgs([num1, num2]))
+    throw new Error('Function arguments are not valid');
+
+  if (num1 < num2) {
+    from = num1;
+    to = num2;
+  }
+  if (num2 < num1) {
+    from = num2;
+    to = num1;
+  }
+  return array.filter(number => number >= from && number <= to);
 }
+
+// --- Third Task ---
+const myIterable = { from: 1, to: 5 };
+myIterable[Symbol.iterator] = function* () {
+  const to = myIterable.to;
+  const from = myIterable.from;
+  if (to < from) throw new Error('Error');
+  for (let i = from; i <= to; i += 1) {
+    yield i;
+  }
+};
