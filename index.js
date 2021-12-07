29d26f91e72d587fba78510829100cdcc18fb878
Olek Myron <myrondell@gmail.com>
2021-12-07 20:26:22 +0200

    basic

--- index.js
+++ index.js
@@ -1,43 +1,12 @@
-const [btnRed, btnBlue] = document.querySelectorAll('.btn');
+const concatStrings = (str1, separator = '') => {
+  return str2 => {
+    return str3 => {
+      return str4 => {
+        if
+        return str1 + separator + str2 + separator + str3 + separator + str4;
+      };
+    };
+  };
+};
 
-btnRed.addEventListener('click', parseNumber);
-btnBlue.addEventListener('click', addDivideNumbers);
-
-function isInvalid(arg) {
-  const parsedArg = Number(arg);
-  return (
-    typeof parsedArg !== 'number' ||
-    isNaN(parsedArg) ||
-    arg === null ||
-    arg === '' ||
-    arg.trim() === ''
-  );
-}
-
-// --- First Task ---
-function parseNumber() {
-  const int = prompt('Enter a number');
-  const calc = prompt('Enter a system number');
-
-  const [parsedInt, parsedCalc] = [Number(int), Number(calc)];
-
-  if (isInvalid(int) || isInvalid(calc)) {
-    return alert('Incorrect input!');
-  }
-
-  return alert(parsedInt.toString(parsedCalc));
-}
-
-// --- Second Task ---
-function addDivideNumbers() {
-  const firstArg = prompt('Enter a first number');
-  if (isInvalid(firstArg)) return alert('Incorrect input!');
-  const secondArg = prompt('Enter a second number');
-  if (isInvalid(secondArg)) return alert('Incorrect input!');
-
-  const [firstNum, secondNum] = [Number(firstArg), Number(secondArg)];
-  const sum = firstNum + secondNum;
-  const quotient = firstNum / secondNum;
-
-  alert(`Result: ${sum}, ${quotient}`);
-}
+console.log(concatStrings('first')('second')('third')());
